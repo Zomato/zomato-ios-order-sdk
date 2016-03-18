fd7af21a597f9846e138d5340edd618539aa8d72
gurasissingh <gurasis.singh@zomato.com>
2016-03-18 09:53:13 +0530

    Initial Commit

---
+++ zomato-ios-order-sdk/zomato-ios-order-sdk.h
@@ -0,0 +1,19 @@
+//
+//  zomato-ios-order-sdk.h
+//  zomato-ios-order-sdk
+//
+//  Created by Gurasis Singh on 18/03/16.
+//  Copyright © 2016 Zomato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for zomato-ios-order-sdk.
+FOUNDATION_EXPORT double zomato_ios_order_sdkVersionNumber;
+
+//! Project version string for zomato-ios-order-sdk.
+FOUNDATION_EXPORT const unsigned char zomato_ios_order_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zomato_ios_order_sdk/PublicHeader.h>
+
+
