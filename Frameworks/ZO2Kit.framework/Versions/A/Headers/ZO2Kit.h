0999869c3d090b752e13ed1ec1ed5ec01d00c796
gurasissingh <gurasis.singh@zomato.com>
2016-04-06 19:11:48 +0530

    Franmework chnages

---
+++ Frameworks/ZO2Kit.framework/Versions/A/Headers/ZO2Kit.h
@@ -0,0 +1,15 @@
+//
+//  ZO2Kit.h
+//  ZO2Kit
+//
+//  Created by Anurag Kabra on 20/07/15.
+//  Copyright (c) 2015 Zomato Media Pvt. Ltd. All rights reserved.
+//
+
+#import <ZO2Kit/OnlineOrderingViewController.h>
+
+#import <ZO2Kit/O2CreditCardSelectionViewController.h>
+#import <ZO2Kit/WalletViewController.h>
+
+//#import <ZO2Kit/OnlineOrdersHistoryViewController.h>
+//#import <ZO2Kit/AllDeliveryAddressesViewController.h>
